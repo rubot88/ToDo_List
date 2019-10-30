8d311b30572864f3185fe4831de7b592b1868590
Rustam Botirov <rustambotirov88@gmail.com>
2019-10-30 14:46:46 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+
+function App() {
+  return <div className="App" />;
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
