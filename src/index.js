b805ed64ecff9ba504a3f2de137479b6826c0418
Rustam Botirov <rustambotirov88@gmail.com>
2019-10-30 22:23:50 +0200

    fisrst components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { render } from "react-dom";
 
-function App() {
-  return <div className="App" />;
+const ToDoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Awesome App</li>
+    </ul>
+  );
 }
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+const AppHeader = () => {
+  return <h1>My Todo List</h1>
+}
+
+const SearchPanel = () => {
+  return <input placeholder="search" />
+}
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <ToDoList />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
